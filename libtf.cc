24707ba3fd46de74ca6b76c5b3507d63d1f7e273
Kwabena W. Agyeman <kwagyeman@live.com>
2019-04-20 03:27:17 -0400

    Generateion of wrapper module complete

---
+++ libtf.cc
@@ -0,0 +1,96 @@
+/* This file is part of the OpenMV project.
+ * Copyright (c) 2013-2019 Ibrahim Abdelkader <iabdalkader@openmv.io> & Kwabena W. Agyeman <kwagyeman@openmv.io>
+ * This work is licensed under the MIT license, see the file LICENSE for details.
+ */
+ 
+#include "tensorflow/lite/experimental/micro/kernels/all_ops_resolver.h"
+#include "tensorflow/lite/experimental/micro/micro_error_reporter.h"
+#include "tensorflow/lite/experimental/micro/micro_interpreter.h"
+#include "tensorflow/lite/schema/schema_generated.h"
+#include "tensorflow/lite/version.h"
+#include "libtf.h"
+
+extern "C" {
+    int run_classification(const unsigned char *model_data,
+                           uint8_t *tensor_arena, const int tensor_arena_size,
+                           uint8_t *input_data, const int input_height, const int input_width, const int input_channels, 
+                           float *class_scores, const int class_scores_size) {
+        // Set up logging.
+        tflite::MicroErrorReporter micro_error_reporter;
+        tflite::ErrorReporter* error_reporter = &micro_error_reporter;
+        
+        // Map the model into a usable data structure. This doesn't involve any
+        // copying or parsing, it's a very lightweight operation.
+        const tflite::Model* model = ::tflite::GetModel(model_data);
+        if (model->version() != TFLITE_SCHEMA_VERSION) {
+          error_reporter->Report(
+              "Model provided is schema version %d not equal to supported version %d.\n",
+              model->version(), TFLITE_SCHEMA_VERSION);
+          return 1;
+        }
+
+        // This pulls in all the operation implementations we need.
+        tflite::ops::micro::AllOpsResolver resolver;
+
+        // Create an area of memory to use for input, output, and intermediate arrays.
+        // The size of this will depend on the model you're using, and may need to be
+        // determined by experimentation.
+        tflite::SimpleTensorAllocator tensor_allocator(tensor_arena, tensor_arena_size);
+
+        // Build an interpreter to run the model with.
+        tflite::MicroInterpreter interpreter(model, resolver, &tensor_allocator, error_reporter);
+
+        // Get information about the memory area to use for the model's input.
+        TfLiteTensor* model_input = interpreter.input(0);
+
+        if ((model_input->dims->size != 4) ||
+            (model_input->dims->data[0] != 1) ||
+            (model_input->dims->data[1] != input_height) ||
+            (model_input->dims->data[2] != input_width) ||
+            (model_input->dims->data[3] != input_channels) ||
+            (model_input->type != kTfLiteUInt8)) {
+          error_reporter->Report("Bad input tensor parameters in model!\n");
+          return 1;
+        }
+
+        // Initialize the feature data.
+        model_input->data.uint8 = input_data;
+
+        // Run the model on the input and make sure it succeeds.
+        if (interpreter.Invoke() != kTfLiteOk) {
+          error_reporter->Report("Invoke failed!\n");
+          return 1;
+        }
+
+        // The output from the model is a vector containing the scores for each kind of prediction.
+        TfLiteTensor* output = interpreter.output(0);
+
+        if ((output->dims->size != 2) ||
+            (output->dims->data[0] != 1) ||
+            (output->dims->data[1] != class_scores_size)) {
+          error_reporter->Report(
+              "The results for recognition should contain %d elements, but there are %d in an %d-dimensional shape.\n",
+              class_scores_size, output->dims->data[1], output->dims->size);
+          return 1;
+        }
+
+        if (output->type != kTfLiteUInt8) {
+          error_reporter->Report(
+              "The results for recognition should be uint8 elements, but are %d.\n",
+              output->type);
+          return 1;
+        }
+
+        int sum = 0;
+        for (int i = 0; i < class_scores_size; ++i) {
+          sum += output->data.uint8[i];
+        }
+
+        for (int i = 0; i < class_scores_size; ++i) {
+          class_scores[i] = sum ? (output->data.uint8[i] / sum) : 0;
+        }
+
+        return 0;
+    }
+}
+
--- make.py
+++ make.py
@@ -11,6 +11,9 @@ def make():
     parser = argparse.ArgumentParser(description =
     "Make Script")
 
+    parser.add_argument("-s", "--skip_generate_projects", action='store_true', default=False,
+    help = "Skip generate_projects")
+
     args = parser.parse_args()
 
     ###########################################################################
@@ -22,9 +25,10 @@ def make():
     if not os.path.exists(builddir):
         os.mkdir(builddir)
 
-    if os.system("cd tensorflow" +
-    " && make -f tensorflow/lite/experimental/micro/tools/make/Makefile TAGS=\"openmvcam\" generate_projects"):
-        sys.exit("Make Failed...")
+    if not args.skip_generate_projects:
+        if os.system("cd tensorflow" +
+        " && make -f tensorflow/lite/experimental/micro/tools/make/Makefile TAGS=\"openmvcam\" generate_projects"):
+            sys.exit("Make Failed...")
 
     # OPENMV1 #################################################################
 
@@ -36,7 +40,7 @@ def make():
     data = None
     with open(os.path.join(builddir, "OPENMV1/Makefile"), 'r') as original:
         data = original.read()
-        data = data.replace("  tensorflow/lite/experimental/micro/examples/micro_speech/main.cc", " ")
+        data = data.replace("  tensorflow/lite/experimental/micro/examples/micro_speech/main.cc", " libtf.cc")
         data = data.replace("$(CXX) $(CXXFLAGS) -o $@ $(OBJS) $(LDFLAGS)", "arm-none-eabi-ar rcs libtf.a $<")
 
     with open(os.path.join(builddir, "OPENMV1/Makefile"), 'w') as modified:
@@ -47,6 +51,9 @@ def make():
         modified.write("CCFLAGS += -mcpu=cortex-m4 -mtune=cortex-m4 -mfpu=fpv4-sp-d16 -mfloat-abi=hard\n")
         modified.write("CXXFLAGS += -mcpu=cortex-m4 -mtune=cortex-m4 -mfpu=fpv4-sp-d16 -mfloat-abi=hard\n")
         modified.write(data)
+
+    shutil.copy(os.path.join(__folder__, "libtf.cc"), os.path.join(builddir, "OPENMV1"))
+    shutil.copy(os.path.join(__folder__, "libtf.h"), os.path.join(builddir, "OPENMV1"))
 
     if os.system("cd " + os.path.join(builddir, "OPENMV1") + " && make -j" + str(cpus)):
         sys.exit("Make Failed...")
@@ -61,7 +68,7 @@ def make():
     data = None
     with open(os.path.join(builddir, "OPENMV2/Makefile"), 'r') as original:
         data = original.read()
-        data = data.replace("  tensorflow/lite/experimental/micro/examples/micro_speech/main.cc", " ")
+        data = data.replace("  tensorflow/lite/experimental/micro/examples/micro_speech/main.cc", " libtf.cc")
         data = data.replace("$(CXX) $(CXXFLAGS) -o $@ $(OBJS) $(LDFLAGS)", "arm-none-eabi-ar rcs libtf.a $<")
 
     with open(os.path.join(builddir, "OPENMV2/Makefile"), 'w') as modified:
@@ -72,6 +79,9 @@ def make():
         modified.write("CCFLAGS += -mcpu=cortex-m4 -mtune=cortex-m4 -mfpu=fpv4-sp-d16 -mfloat-abi=hard\n")
         modified.write("CXXFLAGS += -mcpu=cortex-m4 -mtune=cortex-m4 -mfpu=fpv4-sp-d16 -mfloat-abi=hard\n")
         modified.write(data)
+
+    shutil.copy(os.path.join(__folder__, "libtf.cc"), os.path.join(builddir, "OPENMV2"))
+    shutil.copy(os.path.join(__folder__, "libtf.h"), os.path.join(builddir, "OPENMV2"))
 
     if os.system("cd " + os.path.join(builddir, "OPENMV2") + " && make -j" + str(cpus)):
         sys.exit("Make Failed...")
@@ -86,7 +96,7 @@ def make():
     data = None
     with open(os.path.join(builddir, "OPENMV3/Makefile"), 'r') as original:
         data = original.read()
-        data = data.replace("  tensorflow/lite/experimental/micro/examples/micro_speech/main.cc", " ")
+        data = data.replace("  tensorflow/lite/experimental/micro/examples/micro_speech/main.cc", " libtf.cc")
         data = data.replace("$(CXX) $(CXXFLAGS) -o $@ $(OBJS) $(LDFLAGS)", "arm-none-eabi-ar rcs libtf.a $<")
 
     with open(os.path.join(builddir, "OPENMV3/Makefile"), 'w') as modified:
@@ -97,6 +107,9 @@ def make():
         modified.write("CCFLAGS += -mcpu=cortex-m7 -mtune=cortex-m7 -mfpu=fpv5-sp-d16 -mfloat-abi=hard\n")
         modified.write("CXXFLAGS += -mcpu=cortex-m7 -mtune=cortex-m7 -mfpu=fpv5-sp-d16 -mfloat-abi=hard\n")
         modified.write(data)
+
+    shutil.copy(os.path.join(__folder__, "libtf.cc"), os.path.join(builddir, "OPENMV3"))
+    shutil.copy(os.path.join(__folder__, "libtf.h"), os.path.join(builddir, "OPENMV3"))
 
     if os.system("cd " + os.path.join(builddir, "OPENMV3") + " && make -j" + str(cpus)):
         sys.exit("Make Failed...")
@@ -111,7 +124,7 @@ def make():
     data = None
     with open(os.path.join(builddir, "OPENMV4/Makefile"), 'r') as original:
         data = original.read()
-        data = data.replace("  tensorflow/lite/experimental/micro/examples/micro_speech/main.cc", " ")
+        data = data.replace("  tensorflow/lite/experimental/micro/examples/micro_speech/main.cc", " libtf.cc")
         data = data.replace("$(CXX) $(CXXFLAGS) -o $@ $(OBJS) $(LDFLAGS)", "arm-none-eabi-ar rcs libtf.a $<")
 
     with open(os.path.join(builddir, "OPENMV4/Makefile"), 'w') as modified:
@@ -123,8 +136,13 @@ def make():
         modified.write("CXXFLAGS += -mcpu=cortex-m7 -mtune=cortex-m7 -mfpu=fpv5-sp-d16 -mfloat-abi=hard\n")
         modified.write(data)
 
+    shutil.copy(os.path.join(__folder__, "libtf.cc"), os.path.join(builddir, "OPENMV4"))
+    shutil.copy(os.path.join(__folder__, "libtf.h"), os.path.join(builddir, "OPENMV4"))
+
     if os.system("cd " + os.path.join(builddir, "OPENMV4") + " && make -j" + str(cpus)):
         sys.exit("Make Failed...")
+
+    ###########################################################################
 
 if __name__ == "__main__":
     make()
