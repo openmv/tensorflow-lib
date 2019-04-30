86d7c35e2f10507f91a7d63c84455be2677e2e2c
Kwabena W. Agyeman <kwagyeman@live.com>
2019-04-30 16:35:38 -0700

    Fixed alot of compile issues

--- make.py
+++ make.py
@@ -21,13 +21,13 @@ def generate(target, __folder__, cpus, builddir, bindir, compile_flags):
     with open(os.path.join(builddir, target, "Makefile"), 'r') as original:
         data = original.read()
         data = data.replace("  tensorflow/lite/experimental/micro/examples/micro_speech/main.cc", " libtf.cc")
-        data = data.replace("$(CXX) $(CXXFLAGS) -o $@ $(OBJS) $(LDFLAGS)", "arm-none-eabi-ar rcs libtf.a $<")
+        data = data.replace("--std=c++11 -g -DTF_LITE_STATIC_MEMORY", "--std=c++11 -DTF_LITE_STATIC_MEMORY")
+        data = data.replace("-DNDEBUG -g -DTF_LITE_STATIC_MEMORY", "-DNDEBUG -DTF_LITE_STATIC_MEMORY")
+        data = data.replace("$(CXX) $(CXXFLAGS) -o $@ $(OBJS) $(LDFLAGS)", "arm-none-eabi-ar rcs libtf.a $(OBJS)")
 
     with open(os.path.join(builddir, target, "Makefile"), 'w') as modified:
         modified.write("CC = arm-none-eabi-gcc\n")
         modified.write("CXX = arm-none-eabi-g++\n")
-        modified.write("CCFLAGS += -mthumb -nostartfiles -fdata-sections -ffunction-sections\n")
-        modified.write("CXXFLAGS += -mthumb -nostartfiles -fdata-sections -ffunction-sections\n")
         modified.write("CCFLAGS += " + compile_flags + "\n")
         modified.write("CXXFLAGS += " + compile_flags + "\n")
         modified.write(data)
@@ -38,7 +38,7 @@ def generate(target, __folder__, cpus, builddir, bindir, compile_flags):
     shutil.copy(os.path.join(__folder__, "libtf-mobilenet.h"), os.path.join(builddir, target))
 
     if os.system("cd " + os.path.join(builddir, target) + " && make -j" + str(cpus) + 
-        " && arm-none-eabi-gcc -o libtf-mobilenet.o -c libtf-mobilenet.c" +
+        " && arm-none-eabi-gcc " + compile_flags + " -o libtf-mobilenet.o -c libtf-mobilenet.c" +
         " && arm-none-eabi-ar rcs libtf-mobilenet.a libtf-mobilenet.o"):
         sys.exit("Make Failed...")
 
@@ -50,16 +50,16 @@ def generate(target, __folder__, cpus, builddir, bindir, compile_flags):
     shutil.copy(os.path.join(__folder__, "libtf-mobilenet.h"), os.path.join(bindir, target))
 
     with open(os.path.join(bindir, target, "README.txt"), 'w') as file:
-        file.write("Compiled with -mthumb -nostartfiles -fdata-sections -ffunction-sections " + compile_flags + "\n")
+        file.write("Compiled with " + compile_flags + "\n")
         file.write("Make sure to link this library with arm-none-eabi-g++ as it was written in C++.\n")
         file.write("Finally, this library outputs debugging information via printf() (so printf() must be implemented on your system).\n")
 
 def build_target(target, __folder__, cpus, builddir, bindir):
 
     if (target == "OPENMV1") or (target == "OPENMV2") or (target == "cortex-m4"):
-        generate(target, __folder__, cpus, builddir, bindir, "-mcpu=cortex-m4 -mtune=cortex-m4 -mfpu=fpv4-sp-d16 -mfloat-abi=hard")
+        generate(target, __folder__, cpus, builddir, bindir, "-mthumb -nostartfiles -fdata-sections -ffunction-sections -mcpu=cortex-m4 -mtune=cortex-m4 -mfpu=fpv4-sp-d16 -mfloat-abi=hard")
     elif (target == "OPENMV3") or (target == "OPENMV4") or (target == "cortex-m7"):
-        generate(target, __folder__, cpus, builddir, bindir, "-mcpu=cortex-m7 -mtune=cortex-m7 -mfpu=fpv5-sp-d16 -mfloat-abi=hard")
+        generate(target, __folder__, cpus, builddir, bindir, "-mthumb -nostartfiles -fdata-sections -ffunction-sections -mcpu=cortex-m7 -mtune=cortex-m7 -mfpu=fpv5-sp-d16 -mfloat-abi=hard")
     else:
         sys.exit("Unknown target!")
 
